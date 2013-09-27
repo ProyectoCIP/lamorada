85848bf0db9add6e58a76d17ba89bc2c1e4220b5
Ezequiel Celiz <eceliz@MCG-NQN101-99.MCG-NQN>
2013-09-27 13:04:37 -0300

    Corregido archivos basura en el repositorio

---
+++ webapp/src/main/java/app/ContactoVOPagePanel.java
@@ -0,0 +1,24 @@
+package app;
+
+import org.apache.isis.viewer.wicket.ui.pages.entity.EntityPage;
+import org.apache.isis.viewer.wicket.ui.panels.PanelAbstract;
+import org.apache.wicket.Component;
+
+public class ContactoVOPagePanel extends PanelAbstract<> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7034354524600963726L;
+
+	public ContactoVOPagePanel(String id, EntityPage entityPageModel) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onRender() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
